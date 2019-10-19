31f469e49a2a2a78f93f3a42303d3758e4db2177
Solodovnikova Ekaterina <colo18@yandex.ru>
2019-10-19 16:32:44 +0500

    added reset button and fixed form submit issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,6 @@
 <script>
 	import axios from "axios";
 	import { onMount } from 'svelte';
-	let fa;
-	let ca;
-	let ia;
-	let fd;
-	let cd;
-	let id;
 	let sessions = [];
 
 	onMount(async () => {
@@ -42,14 +36,14 @@
 		width: 210px;
 		border-radius: 5px;
 		margin-top: 10px;
+		display: block;
 	}
 
 </style>
 
 <form action='https://realtimepoll-server.herokuapp.com/results' 
 	oninput='document.getElementsByName("output")[0].src="about:blank"' 
-	method="POST" 
-	onsubmit="this.submit(); this.reset();">
+	method="POST" >
 	<div class='header'>
 		<div class='selectSession'>
 			Выберите сессию
@@ -67,24 +61,25 @@
 	<table class='dataInputs'>
 	<tr>
 		<td>Ф(А)</td>
-		<td><input type=number bind:value={fa} name="fa" min=0 max=10></td>
+		<td><input type=number name="fa" min=0 max=10></td>
 		<td>Ф(Д)</td>
-		<td><input type=number bind:value={fd} name="fd" min=0 max=10 value="5"></td>
+		<td><input type=number name="fd" min=0 max=10></td>
 	</tr>
 	<tr>
 		<td>C(А)</td>
-		<td><input type=number bind:value={ca} name="ca" min=0 max=10></td>
+		<td><input type=number name="ca" min=0 max=10></td>
 		<td>C(Д)</td>
-		<td><input type=number bind:value={cd} name="cd" min=0 max=10 value="5"></td>
+		<td><input type=number name="cd" min=0 max=10></td>
 	</tr>
 	<tr>
 		<td>И(А)</td>
-		<td><input type=number bind:value={ia} name="ia" min=0 max=10 value="5"></td>
+		<td><input type=number name="ia" min=0 max=10 ></td>
 		<td>И(Д)</td>
-		<td><input type=number bind:value={id} name="id" min=0 max=10 value="5"></td>
+		<td><input type=number name="id" min=0 max=10></td>
 	</tr>
 	</table>
 
+	<button type="reset">Очистить</button>
 	<button type="submit" formtarget="output">Готово</button>
 </form>
 
